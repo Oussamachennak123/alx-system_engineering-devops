3ff5d00030333cdac9c0c2e97e15aa4bb665b1f7
Oussama <chennak.oussama@gmail.com>
2024-02-28 00:32:05 +0100

    create a Ruby script that accepts one argument and pass it to a regular expression matching method

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 #  the project instructions, create a Ruby script that accepts one argument
-#  and pass it to a regular expression matching method
+
 print ARGV[0].scan(/hbt{2,5}n/).join
