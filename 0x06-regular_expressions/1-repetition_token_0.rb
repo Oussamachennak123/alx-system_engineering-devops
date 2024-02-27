75e3a5a341de8e2a3f380012acb0e6b3df63fc68
Oussama <chennak.oussama@gmail.com>
2024-02-28 00:03:15 +0100

    Ruby script that accepts one argument

---
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+# project instructions, create a Ruby script that accepts one argument and
+# pass it to a regular expression matching method
+
+print ARGV[0].scan(/hbt{2,5}n/).join
