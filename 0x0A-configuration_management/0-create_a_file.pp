bcc2d0904ae8f656acbfa15bb297f903259902d1
Oussama <chennak.oussama@gmail.com>
2024-04-20 18:00:21 +0100

    Creates a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Creates a file
+file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
+  owner   => 'www-data',
+    mode    => '0744',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
