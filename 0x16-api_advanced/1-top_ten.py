464fd7baabdaa2c0ad0dd9d17b18ab957f2616a5
Oussama <chennak.oussama@gmail.com>
2024-07-01 22:58:27 +0100

    Function to print hot posts on a given Reddit subreddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Function to print hot posts on a given Reddit subreddit."""
+"""Function to print hot posts on a given Reddit subreddit"""
 import requests
 
 
@@ -12,7 +12,8 @@ def top_ten(subreddit):
     params = {
         "limit": 10
     }
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
     if response.status_code == 404:
         print("None")
         return
